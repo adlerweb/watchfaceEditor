f1baf100608ee16b2f55e960ce842fe5d4db081b
Florian Knodt <git@adlerweb.info>
2020-08-13 00:46:07 +0200

    Update devices_list.js

--- dev/js/devices/devices_list.js
+++ dev/js/devices/devices_list.js
@@ -1,12 +1,14 @@
 import bip from './bip.js';
 import cor from './cor.js';
 import band4 from './band4.js';
+import band5 from './band5.js';
 import gtr from './gtr.js';
 
 let devices = {
     bip: bip,
     cor: cor,
     band4: band4,
+    band5: band5,
     gtr: gtr
 };
 
